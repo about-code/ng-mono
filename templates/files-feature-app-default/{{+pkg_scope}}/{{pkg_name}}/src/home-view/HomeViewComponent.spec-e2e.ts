05cf27d5a75387baad5e05c740aa11391e2649f8
about-code <about-code@users.noreply.github.com>
2017-09-16 19:25:38 +0200

    Add Protractor E2E-Testing with sample

---
+++ templates/files-feature-app-default/{{+pkg_scope}}/{{pkg_name}}/src/home-view/HomeViewComponent.spec-e2e.ts
@@ -0,0 +1,19 @@
+import {browser, by} from 'protractor';
+
+describe('AppComponent', () => {
+
+    it('should display "Welcome Home"', (done) => {
+        browser.get('http://localhost:8080<%= app_ctx_root %>');
+        browser
+            .findElement(by.xpath('.//h1'))
+            .then(el => el.getText())
+            .then(innerText => {
+                expect(innerText).toBe('Welcome Home', 'Wrong inner text. Did HomeViewComponent-Template change?');
+                done();
+            })
+            .catch((err) => {
+                fail(err);
+                done();
+            });
+    });
+});
---
+++ templates/files-project-package/config/protractor.js
@@ -0,0 +1,35 @@
+let path = require('path');
+
+// An example configuration file
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumArgs: [],
+  jvmArgs: [],
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    browserName: 'chrome'
+  },
+
+  // Spec patterns are relative to the configuration file location passed
+  // to protractor (in this example conf.js).
+  // They may include glob patterns.
+  specs: [
+      '../src/**/*e2e.{ts,js}',
+      '../packages/**/*e2e.{ts,js}'
+  ],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+    defaultTimeoutInterval: 15000
+  },
+
+  beforeLaunch: function() {
+      require('ts-node').register({
+          project: path.resolve(__dirname, '../'),
+          compiler: 'typescript'
+      });
+  }
+};
+
