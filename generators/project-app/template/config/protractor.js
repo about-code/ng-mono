de895cbae7e92bb66b374144be510585079b35d9
about-code <about-code@users.noreply.github.com>
2018-07-08 13:35:59 +0200

    Fix: Use `.e2e.ts` pattern to remain consistent with other configs

--- generators/project-app/template/config/protractor.js
+++ generators/project-app/template/config/protractor.js
@@ -4,36 +4,32 @@ let path = require('path');
 exports.config = {
   // The address of a running selenium server.
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumArgs: [],
-  jvmArgs: [],
+  seleniumArgs: []
+  ,jvmArgs: []
   // Capabilities to be passed to the webdriver instance.
-  capabilities: {
+  ,capabilities: {
     browserName: 'chrome'
-  },
-
+  }
   // Spec patterns are relative to the configuration file location passed
   // to protractor (in this example conf.js).
   // They may include glob patterns.
-  specs: [
-      '../src/**/*-e2e.{ts,js}',
-      '../packages/**/*-e2e.{ts,js}'
-  ],
-
+  ,specs: [
+      '../src/**/*.e2e.{ts,js}'
+      ,'../packages/**/*.e2e.{ts,js}'
+  ]
   // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true, // Use colors in the command line report.
-    defaultTimeoutInterval: 15000
-  },
-
-  beforeLaunch: function() {
+  ,jasmineNodeOpts: {
+    showColors: true // Use colors in the command line report.
+    ,defaultTimeoutInterval: 15000
+  }
+  ,beforeLaunch: function() {
     require('ts-node').register({
-      project: path.resolve(__dirname, '../tsconfig.json'),
-      compiler: 'typescript',
-      transpileOnly: true,
-      compilerOptions: {
+      project: path.resolve(__dirname, '../tsconfig.json')
+      ,compiler: 'typescript'
+      ,transpileOnly: true
+      ,compilerOptions: {
         module: "commonjs"
       }
     });
   }
 };
-
